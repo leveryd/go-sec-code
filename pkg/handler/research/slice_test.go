863f0dd24c4d0cf499c223780bd6a84687610ab8
leveryd <leveryd@leveryd.com>
2022-04-16 21:00:51 +0800

    修改：两个错误的例子
    https://github.com/leveryd/go-sec-code/issues/2
    
    https://github.com/leveryd/go-sec-code/issues/1

--- pkg/handler/research/slice_test.go
+++ pkg/handler/research/slice_test.go
@@ -10,7 +10,7 @@ func TestSlice(t *testing.T) {
 	a = append(a, 1)
 	a = append(a, 2)
 	a = append(a, 3)
-	a = append(a, 4)
+	// a = append(a, 4)
 
 	b := append(a, 5)
 	c := append(a, 6)
@@ -21,6 +21,8 @@ func TestSlice(t *testing.T) {
 
 	//result:
 	//[1 2 3]
-	//[1 2 3 5]
-	//[1 2 3 5]
+	//[1 2 3 6]
+	//[1 2 3 6]
 }
+
+// https://bycsec.top/2021/02/07/golang%E7%9A%84%E4%B8%80%E4%BA%9B%E5%AE%89%E5%85%A8%E9%97%AE%E9%A2%98/
--- pkg/handler/safe/exec.go
+++ pkg/handler/safe/exec.go
@@ -3,6 +3,7 @@ package safe
 import (
 	"github.com/gin-gonic/gin"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
@@ -13,13 +14,30 @@ func DigHost(c *gin.Context) {
 		return
 	}
 
-	// safe
+	// safe: whitelist
+	{
+		compile, err := regexp.Compile(`^([\w\d.\-_]+)$`)
+		if err != nil {
+			return
+		}
+		if compile.Match([]byte(host)) == false {
+			c.JSON(200, "invalid host arg")
+			return
+		}
+	}
+
 	command := "dig " + host
 	commandResults, err := exec.Command(strings.Split(command, " ")[0], strings.Split(command, " ")[1:]...).Output()
 	//commandResults, err := exec.Command(command).Output()	//
+
 	if err != nil {
 		c.JSON(200, "fail to execute")
 		return
 	}
 	c.Writer.Write(commandResults)
 }
+
+// https://github.com/leveryd/go-sec-code/issues/2
+// 即使没有使用bash -c，也需要注意命令可能有参数能够被用来实施攻击
+
+// 或许需要补充其他类型的安全写法，除了黑名单
